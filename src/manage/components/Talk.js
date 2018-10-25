1c15eca68612356e4c2ec866d412819225530f12
zhaoyongsheng <zhaoyongsheng1@xiaomi.com>
2018-10-25 18:33:46 +0800

    little code

--- src/manage/components/Talk.js
+++ src/manage/components/Talk.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Form, Upload, Input, DatePicker, Checkbox, Icon } from 'antd'
+
+const FormItem = Form.Item
 
 class Talk extends Component {
 
@@ -8,8 +11,17 @@ class Talk extends Component {
   }
 
   render() {
-    return (<div className="talk">I am talk!</div>)
+    const { getFieldDecorator } = this.props.form
+    return (<div className="talk">
+      <Form className="collection-form">
+        <FormItem>{
+          getFieldDecorator('userName', {
+            rules: [{ required: true, message: "Please input your username!"}]
+          })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} placeholder="Username" />} />)
+        }</FormItem>
+      </Form>
+    </div>)
   }
 }
 
-export default Talk
+export default Form.create()(Talk)
