9f4e7bf1ce7652607d929f315860036973b4b8a2
zhaoyongsheng <zhaoyongsheng1@xiaomi.com>
2018-11-02 18:21:30 +0800

    feat: new feat of mongodb

--- server/index.js
+++ server/index.js
@@ -4,11 +4,29 @@ const path = require('path')
 const fileServer = require('koa-static')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
+const mongoose = require('mongoose')
 
 const { uploadFile } = require('./src/utils')
 
 const app = new Koa()
 const router = new Router()
+
+mongoose.connect('mongodb://127.0.0.1/memory')
+const memoryDb = mongoose.connection
+
+memoryDb.on('error', () => {
+  console.log('connection error!')
+})
+
+memoryDb.once('open', () => {
+  console.log('connect successfully!')
+})
+
+const memorySchema = new mongoose.Schema({
+  name: String
+})
+
+const memory = mongoose.model('memory', memorySchema)
 
 app.use(fileServer(path.join(__dirname, './')))
 
@@ -26,6 +44,23 @@ router.post('/imgs/uploads', async (ctx) => {
   result = await uploadFile(ctx, {
     fileType: 'album',
     path: serverFilePath
+  })
+  ctx.body = result
+})
+
+router.post('/story/info', async ctx => {
+  const { name } = ctx.request.body
+  const memoryItem = new memory({ name })
+  const result = { code: 0, message: 'failed'}
+  await new Promise((resolve, reject) => {
+    memoryItem.save((err, item) => {
+      if (err) {
+        return console.log('save failed!')
+      }
+      result.code = 1
+      result.message = 'success'
+      resolve()
+    })
   })
   ctx.body = result
 })
